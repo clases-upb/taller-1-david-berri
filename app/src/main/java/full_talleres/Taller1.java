3d79e76d742711ef4d656eb4bc6fb048ae986cd1
david-berri <david.berrio@upb.edu.co>
2024-08-01 20:20:15 -0500

    1.entrega Taller 1 - David Berrio.

--- app/src/main/java/full_talleres/Taller1.java
+++ app/src/main/java/full_talleres/Taller1.java
@@ -38,6 +38,7 @@ public class Taller1 {
         System.out.println(Calcular_peso_luna((byte)80));
 
     }
+
      /*2.	Diseñe un algoritmo e implemente la función Convertir_km_seg que: reciba una velocidad entera en Km/seg 
      y la convierta a metros/seg y a metros/hora. Retorne un string del tipo: ### m/s - ### m/h. Si hay error, 
      devuelva el string "Error en la conversion"
@@ -59,6 +60,7 @@ public class Taller1 {
 
             }
     }
+
     /*3. Diseñe un algoritmo e implemente la función Convertir_cm_lt que: reciba una cantidad double expresada en 
     cc (centímetros cúbicos) y devuelva un float con su equivalente en litros. Si hay algún error, retorne 0.
     */
@@ -75,6 +77,7 @@ public class Taller1 {
 
             }
     }
+
    /*4.	Diseñe un algoritmo e implemente la función Convertir_us_cops que reciba una cantidad entera de dólares 
    y devuelva su equivalente en pesos usando una TRM de $4170 pesos por cada dólar. La función recibe enteros
    y devuelve enteros, pueden ser grandes. Si hay algún error, retorne -1.
@@ -94,6 +97,7 @@ public class Taller1 {
             }
 
     }
+
    /*5.	Diseñe un algoritmo e implemente la función Convertir_cent_far que recibe la temperatura real en grados 
    centígrados y la devuelve en grados Fahrenheit (averiguar la fórmula) F = 32 + ( 9 * C / 5). Si hay algún error, 
    retorne 0.
@@ -112,6 +116,7 @@ public class Taller1 {
 
             }
     }
+
    /*6.	Diseñe un algoritmo e implemente la función Calcular_segs que recibirá el número de Días, el número de horas, 
    el número de minutos y número segundos como enteros bytes, positivos y devuelva todo en segundos en un entero. 
    Si hay algún error, devuelva -1.
@@ -134,6 +139,7 @@ public class Taller1 {
 
             }
     }
+
    /*7.	Un usuario tiene un sistema de báscula para pesar camiones. Diseñe un algoritmo e implemente la función 
    Calcular_peso_carga que reciba un float con el peso total del camión cargado en toneladas y otro float con 
    lo que pesa el camión vacío en toneladas, y devuelva el peso neto de la carga en kilos y toneladas 
@@ -157,6 +163,7 @@ public class Taller1 {
 
             }
     }
+
    /*8.	Diseñe un algoritmo e implemente la función Calcular_horasxviaje que calcule y devuelva un float con las horas  
    necesarias para alcanzar un destino que es recibido en un string, además de un short con la distancia en kms
    otro dato short que es la velocidad  promedio que alcanzará también el vehículo en kilómetros/hora.  
@@ -176,6 +183,7 @@ public class Taller1 {
             }
 
     }
+    
    /*9.	Un avión necesita cargar combustible para cubrir sus rutas programadas en el día. 
    Cada 0.2 toneladas de combustible puede recorrer 60.8 Km en velocidad de crucero. 
    En el despegue el avión consume 1.2 toneladas de combustible y en el aterrizaje consume 0.4 toneladas. 
@@ -209,6 +217,7 @@ public class Taller1 {
             }
 
     }
+
    /*10. Diseñe un algoritmo e implemente la función Calcular_peso_luna que recibe un byte con el peso en la tierra en kilos
    y devuelve un double el equivalente de ese peso en la luna en Newtons. Utilice las siguientes fórmulas.
    peso_tierra_new = peso_kilos * 9.81m/s2
